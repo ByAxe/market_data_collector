6619459198a1b05315484f7595d6429c6a395dc7
A.Litvinau <A.Litvinau@novacom.by>
2017-12-26 10:22:33 +0300

    master - Initial commit

---
+++ src/main/java/com/crypto/deep/marketdatacollector/MarketDataCollectorApplication.java
@@ -0,0 +1,12 @@
+package com.crypto.deep.marketdatacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketDataCollectorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/crypto/deep/marketdatacollector/MarketDataCollectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crypto.deep.marketdatacollector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarketDataCollectorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

c74d4d7242c1c69140c1e050d0be1dc51affd665
byaxe <byaxe.sx@gmail.com>
2018-01-15 09:45:51 +0300

    master - Significantly accelerated collecting the data into CSV by using two selection windows instead of one

--- src/main/java/com/crypto/deep/marketdatacollector/service/CSVService.java
+++ src/main/java/com/crypto/deep/marketdatacollector/service/CSVService.java
@@ -13,13 +13,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.crypto.deep.marketdatacollector.core.Utils.*;
 import static com.crypto.deep.marketdatacollector.core.enums.CSV_Type.BTC;
 import static com.crypto.deep.marketdatacollector.core.enums.CSV_Type.USD;
+import static java.time.temporal.ChronoUnit.*;
 
 @Service
 public class CSVService implements ICSVService {
@@ -42,14 +45,12 @@ public class CSVService implements ICSVService {
 
             // Sun Jan 01 2017 11:04:00
             LocalDateTime innerBegin = convertMillsToLocalDateTime(1483268640000L);
-//            LocalDateTime begin = convertMillsToLocalDateTime(1483643342000L);
             // Sun Jan 01 2017 11:08:59
-            LocalDateTime innerEnd = innerBegin.plus(4, ChronoUnit.MINUTES).plus(59, ChronoUnit.SECONDS);
-
-            Set<History> historyList = historyService.findAllBetweenDt(innerBegin, innerEnd);
+            LocalDateTime innerEnd = innerBegin.plus(5, MINUTES).minus(1, MILLIS);
 
             // Выбираем все называния валют, для которых есть данные с начального периода
-            List<String> allNamesDistinct = historyList.stream()
+            List<String> allNamesDistinct = historyService.findAllBetweenDt(innerBegin, innerEnd)
+                    .stream()
                     .map(History::getName)
                     .distinct()
                     .collect(Collectors.toList());
@@ -60,51 +61,75 @@ public class CSVService implements ICSVService {
             // header
             CSVUtils.writeLine(writer, allNamesDistinct);
 
+            // Переменные вынесены за цикл для улучшения производительности
             List<String> resultValuesOrdered;
             Map<String, String> nameAndPrice;
+            Set<History> historySet;
 
+
+            // Внешнее окно выборки из БД
             LocalDateTime outerBegin = LocalDateTime.from(innerBegin);
-            LocalDateTime outerEnd = outerBegin.plus(1, ChronoUnit.MONTHS);
+            LocalDateTime outerEnd = outerBegin.plus(1, MONTHS).minus(1, MILLIS);
 
+            // Внешний цикл выбирает из БД данные за большой промежуток времени
             while (outerBegin.isBefore(THRESHOLD)) {
+                System.out.println("Outer window for: " + outerBegin + " - " + outerEnd);
+                historySet = historyService.findAllBetweenDt(outerBegin, outerEnd);
 
-            }
+                // Внутренний цикл выбирает данные уже из Памяти и режет их на необходимого размера кусочки
+                while (innerBegin.isBefore(outerEnd)) {
+                    System.out.println("\tInner window for: " + innerBegin + " - " + innerEnd);
 
-            while (innerBegin.isBefore(THRESHOLD)) {
-                System.out.println("Range for: " + innerBegin + " - " + innerEnd);
+                    Long innerBeginAsLong = convertLocalDateTimeToMills(innerBegin);
+                    Long innerEndAsLong = convertLocalDateTimeToMills(innerEnd);
 
-                // Собираем в удобную структуру, где есть только ИМЯ - ЦЕНА
-                nameAndPrice = historyService.findAllBetweenDt(innerBegin, innerEnd)
-                        .stream()
-                        .sorted(Comparator.comparing(History::getName))
-                        .filter(distinctByKey(History::getName))
-                        .collect(Collectors.toMap(History::getName,
-                                history -> {
-                                    BigDecimal price = history.getPriceBTC();
-                                    if (type == USD) price = history.getPriceUSD();
-                                    return String.valueOf(price);
-                                }));
+                    // Собираем в удобную структуру, где есть только ИМЯ - ЦЕНА
+                    // Выбрасываем значения с повторяющимися именами для того чтобы не было ошибки идентичности
+                    // при добавлении в Map
+                    nameAndPrice = historySet
+                            .stream()
+                            .filter(h -> h.getDt() >= innerBeginAsLong && h.getDt() < innerEndAsLong)
+//                            .sorted(Comparator.comparing(History::getName))
+                            .filter(distinctByKey(History::getName))
+                            .collect(Collectors.toMap(History::getName,
+                                    history -> {
+                                        BigDecimal price = history.getPriceBTC();
+                                        if (type == USD) price = history.getPriceUSD();
+                                        return String.valueOf(price);
+                                    }));
 
-                resultValuesOrdered = new ArrayList<>(historyList.size());
+                    resultValuesOrdered = new ArrayList<>(allNamesDistinct.size());
 
-                // Вписываем значения в новый лист в точно таком же порядке, в котором сделаны колонки
-                // Иначе, может быть рассинхронизация
-                for (String name : allNamesDistinct) {
-                    resultValuesOrdered.add(nameAndPrice.get(name));
+                    // Вписываем значения в новый лист в точно таком же порядке, в котором сделаны колонки
+                    // И только те валюты, которые уже существовали в самом начале
+                    // Иначе, может быть рассинхронизация
+                    for (String name : allNamesDistinct) {
+                        resultValuesOrdered.add(nameAndPrice.get(name));
+                    }
+
+                    // Записываем строку в файл
+                    CSVUtils.writeLine(writer, resultValuesOrdered);
+
+                    // Перемещаемся по времени на след. внутреннее окно выборки
+                    innerBegin = innerBegin.plus(5, MINUTES);
+                    innerEnd = innerEnd.plus(5, MINUTES);
                 }
 
-                // Записываем строку в файл
-                CSVUtils.writeLine(writer, resultValuesOrdered);
+                // Перемещаемся по времени на след. внешнее окно выборки
+                outerBegin = outerBegin.plus(1, MONTHS);
+                outerEnd = outerEnd.plus(1, MONTHS);
 
-                // Перемещаемся по времени на след. промежуток
-                innerBegin = innerBegin.plus(5, ChronoUnit.MINUTES);
-                innerEnd = innerEnd.plus(5, ChronoUnit.MINUTES);
+                // Сбрасываем внутреннее окно, до начала внешнего
+                innerBegin = LocalDateTime.from(outerBegin);
+                innerEnd = innerBegin.plus(5, MINUTES).minus(1, MILLIS);
             }
+
 
             writer.flush();
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
